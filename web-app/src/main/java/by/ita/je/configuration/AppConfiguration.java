9f6b6d36a85b9b6eba4fb8e9f080aee48e7d3307
AliaksandrSakharuk <sakharukaliaksandr@gmail.com>
2021-12-03 11:59:19 +0300

    add prop

--- web-app/src/main/java/by/ita/je/configuration/AppConfiguration.java
+++ web-app/src/main/java/by/ita/je/configuration/AppConfiguration.java
@@ -2,24 +2,10 @@ package by.ita.je.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestTemplate;
-import java.util.Properties;
 
 @Configuration
 public class AppConfiguration {
-
-    @Bean
-    @Primary
-    public Properties objectPropertiesEmail(){
-        Properties prop = new Properties();
-        prop.put("mail.smtp.auth", "true");
-        prop.put("mail.smtp.starttls.enable", "true");
-        prop.put("mail.smtp.host", "smtp.gmail.com");
-        prop.put("mail.smtp.port", "25");
-        return prop;
-    }
-
     @Bean
     public RestTemplate objectRestTemplate(){
         return new RestTemplate();
---
+++ web-app/src/main/java/by/ita/je/configuration/MailProperties.java
@@ -0,0 +1,24 @@
+package by.ita.je.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import java.util.Properties;
+
+@Configuration
+@ConfigurationProperties(prefix = "mail")
+public class MailProperties {
+
+    @Bean
+    @Primary
+    public Properties objectPropertiesEmail(){
+        Properties prop = new Properties();
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.starttls.enable", "true");
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "25");
+        return prop;
+    }
+}
