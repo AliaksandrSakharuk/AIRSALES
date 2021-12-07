822fa74c169f4e5db36ab1449b0a844863607e33
AliaksandrSakharuk <sakharukaliaksandr@gmail.com>
2021-12-07 11:16:01 +0300

    change config

--- web-app/src/main/java/by/ita/je/configuration/AppConfiguration.java
+++ web-app/src/main/java/by/ita/je/configuration/AppConfiguration.java
@@ -1,5 +1,8 @@
 package by.ita.je.configuration;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -10,4 +13,11 @@ public class AppConfiguration {
     public RestTemplate objectRestTemplate(){
         return new RestTemplate();
     }
+
+        @Bean
+        public ObjectMapper objectMapper(){
+            return new ObjectMapper()
+                    .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                    .registerModule(new JavaTimeModule());
+        }
 }
